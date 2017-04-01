e5f380e418108a4a8ac859bdfbbb82267e27338b
Lucas Bento da Silva <lucas.bsilva@outlook.com>
2017-04-01 20:25:50 -0300

    Add location screen

---
+++ src/Globals.js
@@ -0,0 +1,12 @@
+export default {
+  location: {
+    place: 'SENAI Santa Cecília',
+    address: 'XXXXX, 13 - Bairro, São Paulo/SP',
+    coordinate: {
+      latitude: -23.5365517,
+      longitude: -46.6463026,
+      latitudeDelta: 0.0922,
+      longitudeDelta: 0.0421,
+    },
+  },
+};
--- src/screens/Location.js
+++ src/screens/Location.js
@@ -1,21 +1,73 @@
-import React, { Component } from 'react';
+import React, { PureComponent } from 'react';
 import {
+  ScrollView,
   Text,
-  View
+  StyleSheet,
+  Linking,
+  Platform,
+  Alert,
 } from 'react-native';
+import { MapView } from 'expo';
 
-export default class Location extends Component {
+import Globals from '../Globals';
+
+class Location extends PureComponent {
   static route = {
     navigationBar: {
-      title: 'Location'
-		}
-	}
+      title: 'Como Chegar',
+		},
+	};
+
+  handleOpenAddress = async () => {
+    const location = `${Globals.location.coordinate.latitude},${Globals.location.coordinate.longitude}`;
+    const mapsUrl = (Platform.OS === 'ios') ?
+      `http://maps.apple.com/?ll=${location}`
+      : `geo:${location}`;
+
+    const canOpenMaps = await Linking.canOpenURL(mapsUrl);
+    if (!canOpenMaps) {
+      return Alert.alert('Erro', 'Não foi possível abrir um aplicativo de mapas');
+    }
+
+    // TODO (lucasbento): add loading state before opening the map app
+    Linking.openURL(mapsUrl);
+  };
 
   render() {
     return (
-			<View style={{alignItems: 'center', justifyContent: 'center', flex: 1}}>
-				<Text onPress={this.handlePress}>Location!</Text>
-			</View>
+			<ScrollView contentContainerStyle={styles.container}>
+				<Text style={styles.locationInfo}>
+          A React Conf Brasil acontecerá no {Globals.location.place} ({Globals.location.address})
+        </Text>
+
+        <MapView
+          style={styles.map}
+          initialRegion={Globals.location.coordinate}
+          onPress={this.handleOpenAddress}
+        >
+          <MapView.Marker
+            title="SENAI"
+            coordinate={Globals.location.coordinate}
+          />
+        </MapView>
+			</ScrollView>
     )
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 10,
+  },
+  locationInfo: {
+    textAlign: 'center',
+    fontSize: 16,
+    marginBottom: 20,
+  },
+  map: {
+    height: '50%',
+  },
+});
+
+export default Location;
