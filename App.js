461b82b86ae349580b78abab8a0aaa2209124dba
Heloá Meneses <heloa.meneses@gmail.com>
2017-04-02 15:49:32 -0300

    Adding navigation bar colors

--- App.js
+++ App.js
@@ -4,12 +4,21 @@ import {
   StackNavigation,
 } from '@expo/ex-navigation';
 import Router from './src/navigation/Router';
+import Globals from './src/Globals';
 
 export default class App extends React.Component {
   render() {
     return (
       <NavigationProvider router={Router}>
-        <StackNavigation initialRoute={Router.getRoute('home')} />
+        <StackNavigation
+          initialRoute={Router.getRoute('home')}
+          defaultRouteConfig={{
+            navigationBar: {
+              backgroundColor: Globals.colors.$primary_black,
+              tintColor: Globals.colors.$primary_blue,
+            },
+          }}
+        />
       </NavigationProvider>
     );
   }
