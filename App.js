31fb09fba6654aabdd160c9b50d7299da8026c12
Heloá Meneses <heloa.meneses@gmail.com>
2017-04-23 18:53:25 -0300

    Adding ActionSheet example

--- App.js
+++ App.js
@@ -3,23 +3,26 @@ import {
   NavigationProvider,
   StackNavigation,
 } from '@expo/ex-navigation';
+import { ActionSheetProvider } from '@expo/react-native-action-sheet';
 import Router from './src/navigation/Router';
 import Globals from './src/Globals';
 
 export default class App extends React.Component {
   render() {
     return (
-      <NavigationProvider router={Router}>
-        <StackNavigation
-          initialRoute={Router.getRoute('home')}
-          defaultRouteConfig={{
-            navigationBar: {
-              backgroundColor: Globals.colors.primary_black,
-              tintColor: Globals.colors.primary_blue,
-            },
-          }}
-        />
-      </NavigationProvider>
+      <ActionSheetProvider>
+        <NavigationProvider router={Router}>
+          <StackNavigation
+            initialRoute={Router.getRoute('home')}
+            defaultRouteConfig={{
+              navigationBar: {
+                backgroundColor: Globals.colors.primary_black,
+                tintColor: Globals.colors.primary_blue,
+              },
+            }}
+          />
+        </NavigationProvider>
+      </ActionSheetProvider>
     );
   }
 }
