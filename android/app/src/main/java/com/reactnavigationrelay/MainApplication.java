8bc2485d3095947e5e6f8946a9c2830f135ad374
Luiz Fernando Sousa Camargo <luizepauloxd@gmail.com>
2018-05-02 04:56:31 -0300

    android stuff for RN 54

--- android/app/src/main/java/com/reactnavigationrelay/MainApplication.java
+++ android/app/src/main/java/com/reactnavigationrelay/MainApplication.java
@@ -3,7 +3,6 @@ package com.reactnavigationrelay;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
-import com.horcrux.svg.SvgPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
@@ -23,9 +22,13 @@ public class MainApplication extends Application implements ReactApplication {
     @Override
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
-          new MainReactPackage(),
-            new SvgPackage()
+          new MainReactPackage()
       );
+    }
+
+    @Override
+    protected String getJSMainModuleName() {
+      return "index";
     }
   };
 
