28f19380d1e9cfa366df385732583eb06f044700
Luiz Fernando Sousa Camargo <luizepauloxd@gmail.com>
2018-05-02 01:11:15 -0300

    a new color to theme and a new image

--- src/utils/design/images.js
+++ src/utils/design/images.js
@@ -2,4 +2,5 @@ export const IMAGES = {
   REACT: require('./assets/reactjs.png'),
   FB: require('./assets/fb.png'),
   ARROW: require('./assets/arrow.png'),
+  CLOSE: require('./assets/close.png'),
 };
--- src/utils/design/theme.js
+++ src/utils/design/theme.js
@@ -2,5 +2,6 @@ export default {
   colors: {
     primaryColor: '#00FF19',
     secondaryColor: '#000000',
+    errorViewColor: '#ed2121',
   },
 };
