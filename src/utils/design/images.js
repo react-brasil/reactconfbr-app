828acfd1d62cd0b8821edb52713fff382f6df67b
Luiz Fernando Sousa Camargo <luizepauloxd@gmail.com>
2018-05-07 10:33:11 -0300

    adds new icons

--- src/utils/design/images.js
+++ src/utils/design/images.js
@@ -3,4 +3,5 @@ export const IMAGES = {
   FB: require('./assets/fb.png'),
   ARROW: require('./assets/arrow.png'),
   CLOSE: require('./assets/close.png'),
+  SEARCH: require('./assets/search.png'),
 };
