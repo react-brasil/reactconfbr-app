7f33af6562c61ccce6df7fc00ebd637e73bc70b2
lXSPandora <luizepauloxd@gmail.com>
2018-05-27 14:55:13 -0300

    adds new create event

--- src/screens/Event/EventAdd.js
+++ src/screens/Event/EventAdd.js
@@ -1,11 +1,22 @@
 import * as React from 'react';
-import { SafeAreaView, StatusBar, Platform, ScrollView, TouchableOpacity } from 'react-native';
+import {
+  SafeAreaView,
+  StatusBar,
+  Platform,
+  ScrollView,
+  TouchableOpacity,
+  Dimensions,
+  View,
+} from 'react-native';
 import styled from 'styled-components/native';
 import LinearGradient from 'react-native-linear-gradient';
 import { IMAGES } from '../../utils/design/images';
 import KeyBoardSpacer from 'react-native-keyboard-spacer';
 import DatePicker from 'react-native-modal-datetime-picker';
+import Timeline from 'react-native-timeline-listview';
 import moment from 'moment';
+
+const { width } = Dimensions.get('window');
 
 const Wrapper = styled(LinearGradient).attrs({
   colors: ['rgb(41, 123, 247)', '#651FFF'],
@@ -16,12 +27,9 @@ const Wrapper = styled(LinearGradient).attrs({
 `;
 
 const HeaderContainer = styled.View`
-  position: absolute;
-  top: 10;
-  left: 20;
-  right: 20;
-  background-color: transparent;
-  z-index: 1000;
+  padding: 10px 20px;
+  padding-bottom: 20;
+  z-index: 1000
 `;
 
 const Header = styled.View`
@@ -36,6 +44,7 @@ const HeaderButton = styled.TouchableOpacity`
   align-items: center;
   justify-content: center;
   z-index: 1000;
+  margin-top: 10;
 `;
 
 const CloseIcon = styled.Image.attrs({
@@ -73,7 +82,7 @@ const EventName = styled.TextInput.attrs({
   font-weight: 800;
   width: 88%;
   margin: 20px 25px;
-  margin-top: 80;
+  margin-top: 40;
 `;
 
 const EventDescription = styled.TextInput.attrs({
@@ -141,12 +150,82 @@ const PlusIcon = styled.Image.attrs({
   tint-color: ${props => props.theme.colors.primaryColor};
 `;
 
+const AddIcon = styled.Image.attrs({
+  source: IMAGES.ADD,
+}) `
+  width: 35;
+  height: 35;
+  tint-color: ${props => props.theme.colors.primaryColor};
+`;
+
+
 const MinusIcon = styled.Image.attrs({
   source: IMAGES.MINUS,
 }) `
   width: 20;
   height: 20;
   tint-color: ${props => props.theme.colors.primaryColor};
+`;
+
+const ScheduleList = styled(Timeline).attrs({
+  circleSize: 15,
+  lineColor: 'white',
+  lineWidth: 4,
+  circleColor: 'rgb(0, 188, 255)',
+  showTime: false,
+}) `
+  padding: 10px 20px;
+  flex: 1; 
+`;
+
+const ScheduleBaloon = styled.View`
+  background-color: white;
+  flex-direction: row;
+  align-items: center;
+  padding-horizontal: 20;
+  padding-vertical: 15;
+  border-radius: 30;
+  border-top-left-radius: 0;
+  margin: 10px 0px;
+`;
+
+
+const CommentText = styled.Text`
+  font-size: 16;
+  font-weight: bold;
+  color: rgb(51, 108, 248); 
+  width: ${width - 160};
+`;
+
+const ProfileInitials = styled.View`
+  width: 40;
+  height: 40;
+  border-radius: 20;
+  align-items: center;
+  justify-content: center;
+  background-color: #651FFF
+`;
+
+const InitialsText = styled.Text`
+  color: white;
+  font-size: 24;
+  font-weight: bold
+`;
+
+const AddButton = styled.TouchableOpacity`
+  width: 62;
+  height: 62;
+  border-radius: ${62/2};
+  background-color: white;
+  align-items: center;
+  justify-content: center;
+  margin: 40px 0px;
+`;
+
+const Container = styled.View`
+  width: 100%;
+  align-items: center;
+  justify-content: center;
 `;
 
 type Schedules = { title: string, talker: string, time: string }
@@ -178,7 +257,28 @@ class EventAdd extends React.Component<Props, State> {
       location: { cep: '', coordinates: [0, 0] },
       address: '',
       eventLimit: 10,
-      schedules: [],
+      schedules: [
+        {
+          title: 'Entrada e Credenciamento',
+          talker: null,
+          time: '19:00',
+        },
+        {
+          title: 'React Native for Noobs',
+          talker: 'Jabur',
+          time: '20:00',
+        },
+        {
+          title: 'Coffee Break',
+          talker: null,
+          time: '21:00',
+        },
+        {
+          title: 'Advanced Styled Components',
+          talker: 'Luizinho',
+          time: '22:00',
+        },
+      ],
       errorText: '',
       isDatePickerVisible: false,
     }
@@ -201,10 +301,47 @@ class EventAdd extends React.Component<Props, State> {
     });
   };
 
+  getInitials = name => {
+    return name
+      ? name
+        .split(' ')
+        .slice(0, 2)
+        .map(namePart => namePart.charAt(0).toUpperCase())
+        .join('')
+      : '';
+  };
+
   setDatePicker = () => this.setState({ isDatePickerVisible: !this.isDatePickerVisible });
 
+  renderItem = (schedule: Schedules) => {
+    const { title, talker, time } = schedule;
+
+    if (!talker) {
+      return (
+        <ScheduleBaloon>
+          <View>
+            <CommentText>{title}</CommentText>
+            <CommentText>{time}</CommentText>
+          </View>
+        </ScheduleBaloon>
+      )
+    }
+
+    return (
+      <ScheduleBaloon>
+        <View>
+          <CommentText>{title}</CommentText>
+          <CommentText>{time}</CommentText>
+        </View>
+        <ProfileInitials>
+          <InitialsText>{this.getInitials(talker)}</InitialsText>
+        </ProfileInitials>
+      </ScheduleBaloon>
+    )
+  };
+
   render() {
-    const { name, description, address, date, eventLimit, isDatePickerVisible } = this.state;
+    const { name, description, address, date, eventLimit, isDatePickerVisible, schedules } = this.state;
     return (
       <Wrapper>
         <StatusBar barStyle="light-content" />
@@ -254,9 +391,18 @@ class EventAdd extends React.Component<Props, State> {
               </IncreaseButtons>
             </Row>
           </DateAndLocationRow>
+          <ScheduleList
+            data={schedules}
+            renderDetail={this.renderItem}
+          />
+          <Container>
+            <AddButton>
+              <AddIcon />
+            </AddButton>
+          </Container>
         </ScrollView>
         {Platform.OS === 'ios' && <KeyBoardSpacer />}
-        <DatePicker onCancel={this.setDatePicker} onConfirm={this.handleDatePicked} isVisible={isDatePickerVisible} />
+        <DatePicker onCancel={() => this.setState({ isDatePickerVisible: false })} onConfirm={this.handleDatePicked} isVisible={isDatePickerVisible} />
       </Wrapper>
     )
   }
